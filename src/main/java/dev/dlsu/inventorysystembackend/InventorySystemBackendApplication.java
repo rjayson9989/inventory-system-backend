09e2963e284a607e1b638137b967e885248880dd
Roger Jayson M. Mendez III <roger_mendeziii@dlsu.edu.ph>
2022-09-27 18:56:36 +0800

    Initial Commit

---
+++ src/main/java/dev/dlsu/inventorysystembackend/InventorySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.dlsu.inventorysystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySystemBackendApplication.class, args);
+	}
+
+}
