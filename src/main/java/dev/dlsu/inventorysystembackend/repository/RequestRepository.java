62f09e2048e6089078205c0a629ae3b15e4b12f1
jasonuy1998 <112721513+jasonuy1998@users.noreply.github.com>
2022-12-05 23:21:03 +0800

    added @repository tag

--- src/main/java/dev/dlsu/inventorysystembackend/repository/RequestRepository.java
+++ src/main/java/dev/dlsu/inventorysystembackend/repository/RequestRepository.java
@@ -1,9 +1,11 @@
 package dev.dlsu.inventorysystembackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import dev.dlsu.inventorysystembackend.model.Request;
 
+@Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
 }
